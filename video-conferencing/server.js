e04d7f9c4ab923bd2bb4a993b436618edba6325a
Aguel Satria Wijaya <aguelsw@gmail.com>
2019-11-14 04:28:43 +0700

    add express

--- video-conferencing/server.js
+++ video-conferencing/server.js
@@ -7,12 +7,16 @@ var port = process.env.PORT || 9001;
 var server = require('http'),
     url = require('url'),
     path = require('path'),
+    app = require('express')(),
+    express = require('express'),
     fs = require('fs');
+app.use(express.static('img'));
 
 function serverHandler(request, response) {
     try {
         var uri = url.parse(request.url).pathname,
             filename = path.join(process.cwd(), uri);
+            console.log(filename);
 
         if (filename && filename.search(/server.js/g) !== -1) {
             response.writeHead(404, {
@@ -44,21 +48,38 @@ function serverHandler(request, response) {
             filename += '/index.html';
         }
 
+        if(filename.search("intro-bg.jpg") != -1) {
+            fs.readFile(filename, function (err, content) {
+                if (err) {
+                    response.writeHead(500, {
+                        'Content-Type': 'text/plain'
+                    });
+                    response.write('404 Not Found: ' + path.join('/', uri) + '\n');
+                    response.end();
+                    return;
+                } else {
+                    //specify the content type in the response will be an image
+                    response.writeHead(200,{'Content-type':'image/jpg'});
+                    response.end(content);
+                }
+            });
+        } else {
+            fs.readFile(filename, 'utf8', function(err, file) {
+                if (err) {
+                    response.writeHead(500, {
+                        'Content-Type': 'text/plain'
+                    });
+                    response.write('404 Not Found: ' + path.join('/', uri) + '\n');
+                    response.end();
+                    return;
+                }
 
-        fs.readFile(filename, 'utf8', function(err, file) {
-            if (err) {
-                response.writeHead(500, {
-                    'Content-Type': 'text/plain'
-                });
-                response.write('404 Not Found: ' + path.join('/', uri) + '\n');
+                response.writeHead(200);
+                response.write(file, 'utf8');
                 response.end();
-                return;
-            }
+            });
+        }
 
-            response.writeHead(200);
-            response.write(file, 'utf8');
-            response.end();
-        });
     } catch (e) {
         response.writeHead(404, {
             'Content-Type': 'text/plain'
